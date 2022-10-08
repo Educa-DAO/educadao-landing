12e2cef55d397d7217a8360b5d5b524bb3b90444
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-08 12:33:38 -0300

    chore: change to `BrowserRouter` with `basename`

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 //import './App.css';
-import './assets/scss/style.scss';
+import "./assets/scss/style.scss";
 
 const history = createBrowserHistory();
 
+console.log(process.env.PUBLIC_URL);
+
 ReactDOM.render(
-  <Router history={history}>
+  <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
