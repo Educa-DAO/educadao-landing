c8fc14258cec56c93ad23d3dc2ba986b71744573
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-08 03:43:31 -0300

    style: optimize imports

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 // import sections
-import Hero from '../components/sections/Hero';
-import FeaturesTiles from '../components/sections/FeaturesTiles';
-import FeaturesSplit from '../components/sections/FeaturesSplit';
-import Testimonial from '../components/sections/Testimonial';
-import Cta from '../components/sections/Cta';
+import Hero from "../components/sections/Hero";
+// import FeaturesTiles from '../components/sections/FeaturesTiles';
+// import FeaturesSplit from '../components/sections/FeaturesSplit';
+// import Testimonial from '../components/sections/Testimonial';
+// import Cta from '../components/sections/Cta';
 
 const Home = () => {
-
   return (
     <>
       <Hero className="illustration-section-01" />
@@ -17,6 +16,6 @@ const Home = () => {
       <Cta split /> */}
     </>
   );
-}
+};
 
 export default Home;
