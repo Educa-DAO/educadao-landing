00e9c37dcc5bcdf54c36f59bb1cb2495155b9e90
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-28 13:27:25 -0300

    chore: add `favicon`

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -12,6 +12,7 @@ export default function PrimaryLayout({
   return (
     <>
       <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <title>Educa.DAO</title>
       </Head>
 
