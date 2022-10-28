41ce022fb5d1700333533fbb936d51f815f436fd
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-28 18:46:07 -0300

    chore: add meta-data

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -14,6 +14,31 @@ export default function PrimaryLayout({
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <title>Educa.DAO</title>
+
+        <meta
+          name="description"
+          content="Impulsionadora de conteúdos educacionais, com acesso gratuito e inclusivo para que estudantes se conectem a produtores de conteúdos, tanto consumindo os cursos, quanto sugerindo novos temas."
+        />
+        <meta
+          name="keywords"
+          content="educacao educadao tecnologia dao blockchain educação gratuita certificação certificado avaliação quiz aprender"
+        />
+        <meta name="author" content="EducaDAO" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta
+          property="og:title"
+          content="EducaDAO: O projeto que quer transformar a educação gratuita."
+        />
+        <meta
+          property="og:description"
+          content="Impulsionadora de conteúdos educacionais, com acesso gratuito e inclusivo para que estudantes se conectem a produtores de conteúdos, tanto consumindo os cursos, quanto sugerindo novos temas."
+        />
+        <meta property="og:url" content="https://educa-dao.com/" />
+        <meta property="og:site_name" content="EducaDAO" />
+        <meta
+          property="og:image"
+          content="https://files.gitbook.com/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FcKCM247FiUYX6K0eHvGy%2Fuploads%2Ff6TXInQHWEvrlSae8gCD%2Flogo_ganhadores.png?alt=media&token=4b6eb86a-7a9b-44d1-8bdb-e4c4511599f2"
+        />
       </Head>
 
       <div
