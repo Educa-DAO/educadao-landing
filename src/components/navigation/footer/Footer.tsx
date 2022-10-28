84f9d699e9817727f4c9b5120cc0308c7985ad5e
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-28 19:12:44 -0300

    feat(footer): implement component

--- src/components/navigation/footer/Footer.tsx
+++ src/components/navigation/footer/Footer.tsx
@@ -1,6 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 import { FooterProps } from './Footer.types';
 
 export default function Footer({ className, ...footerProps }: FooterProps) {
-  return <footer {...footerProps}></footer>;
+  return (
+    <footer
+      className="flex flex-col items-center p-10 bg-sun text-daisyBush"
+      {...footerProps}
+    >
+      <div>EducaDAO — 2022</div>
+      <div>
+        <Link
+          className="underline hover:no-underline"
+          href="https://github.com/Educa-DAO/educadao-landing"
+          target="_blank"
+        >
+          Acesse o código-fonte deste site
+        </Link>
+      </div>
+    </footer>
+  );
 }
