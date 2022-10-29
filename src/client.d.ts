e86dceb49042e72b6826386603744f274976c049
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-28 22:11:09 -0300

    chore: add svg support as react components

---
+++ src/client.d.ts
@@ -0,0 +1,7 @@
+declare module '*.svg' {
+  import type { FunctionComponent, SVGProps } from 'react';
+  const ReactComponent: FunctionComponent<
+    SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  export default ReactComponent;
+}
