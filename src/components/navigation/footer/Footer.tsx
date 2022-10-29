999b86155a43eabf6fb3929c21716ff3f5e26baa
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-29 00:57:44 -0300

    feat(footer): add `box-shadow` & remove link
    Co-Authored-By: Vinícius Oliveira Gonçalves <vinicius@oligon.com.br>

--- src/components/navigation/footer/Footer.tsx
+++ src/components/navigation/footer/Footer.tsx
@@ -1,23 +1,13 @@
-import Link from 'next/link';
 import React from 'react';
 import { FooterProps } from './Footer.types';
 
 export default function Footer({ className, ...footerProps }: FooterProps) {
   return (
     <footer
-      className="flex flex-col items-center p-10 bg-sun text-daisyBush"
+      className="flex flex-col items-center p-10 bg-sun text-daisyBush shadow-t-md shadow-white/30"
       {...footerProps}
     >
       <div>EducaDAO — 2022</div>
-      <div>
-        <Link
-          className="underline hover:no-underline"
-          href="https://github.com/Educa-DAO/educadao-landing"
-          target="_blank"
-        >
-          Acesse o código-fonte deste site
-        </Link>
-      </div>
     </footer>
   );
 }
