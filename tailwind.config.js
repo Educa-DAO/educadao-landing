be105644692bca97ebc34232813b1ed40ef206c5
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-29 00:56:59 -0300

    chore(tailwind/config): add `box-shadow` extensions
    Co-Authored-By: Vinícius Oliveira Gonçalves <vinicius@oligon.com.br>

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,16 @@ module.exports = {
         lightGray: '#ECEDED',
         gullGray: '#9CA9B3',
       },
+      boxShadow: {
+        't-sm': '0 -1px 2px 0 rgba(0, 0, 0, 0.05)',
+        't-md': '0px -5px 20px 0px rgb(0 0 0 / 30%)',
+        't-lg':
+          '0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        't-xl':
+          '0 -20px 25px -5px rgb(0 0 0 / 10%), 0 -8px 10px -6px rgb(0 0 0 / 10%)',
+        't-2xl': '0 -25px 50px -12px rgba(0, 0, 0, 0.25)',
+        't-3xl': '0 -35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
     },
   },
   plugins: [require('@tailwindcss/aspect-ratio')],
